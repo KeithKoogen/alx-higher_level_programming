1a68a5afced39333681a46b13ab74f19aba60b1f
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-29 22:38:33 +0200

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
 
     for i in str:
         if ord(i) > 96 and ord(i) < 123:
-            i = i - 32
-            print("{%c}".format(i), end='')
+            ch = ord(i) - 32
+            print("{%c}".format(ch), end='')
         else:
             print("{%c}".format(i), end='')
