242604cbe6799519d0b9cff2413f2f9e2e958824
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 21:56:33 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-arguments = len(argv)
 if __name__ == '__main__':
-    if arguments > 2:
-        print("{:d} arguments:".format(arguments - 1))
-        for i in range(1, arguments):
-            print("{:d}: {:s}".format(i, argv[i]))
+    from sys import argv
+    arguments = len(argv)
+    
     if arguments == 2:
         print("1 argument:")
         print("1: {:s}".format(argv[1]))
-    if arguments < 2:
+    elif arguments > 2:
+        print("{:d} arguments:".format(arguments - 1))
+        for i in range(1, arguments):
+            print("{:d}: {:s}".format(i, argv[i]))
+    else:
         print(f"0 arguments.")
