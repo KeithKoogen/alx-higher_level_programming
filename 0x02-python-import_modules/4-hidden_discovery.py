ee7599811b260729bec4b8d7296651f37f2c6d81
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 22:47:35 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-  import hidden_4
-  module_names = dir(hidden_4)
-  print(module_names)
+    import hidden_4
+    module_names = dir(hidden_4)
+    for i in range(len(module_names)):
+        print(module_names[i])
+  
