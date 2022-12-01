8f0eb4e9a29acae49bd9ba6efba09647efa3f4f7
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 19:58:32 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     if arguments > 2:
         print(f"{arguments - 1} arguments:")
         for i in range(1, arguments):
-            print("{num}: {arg}".format(num = i, arg = argv[i]))
+            print("{num}: {arg}".format(num=i, arg=argv[i]))
 
     if arguments == 2:
         print("1 argument:")
-        print("1: {arg".format(arg = argv[i]))
+        print("1: {arg".format(arg=argv[i]))
 
     if arguments < 2:
         print(f"0 arguments.")
