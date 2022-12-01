fb660005333a487b4e8b4af32120ac1cf412c2a7
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 19:17:52 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,14 @@
-from add_0 import add
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    totalsum = add(a, b)
+    totalsub = sub(a, b)
+    totalmul = mul(a, b)
+    totaldiv = div(a, b)
+    print("{num1} + {num2} = {sum1}".format(num1=a, num2=b, sum1=totalsum))
+    print("{num1} - {num2} = {sum1}".format(num1=a, num2=b, sum1=totalsub))
+    print("{num1} * {num2} = {sum1}".format(num1=a, num2=b, sum1=totalmul))
+    print("{num1} / {num2} = {sum1}".format(num1=a, num2=b, sum1=totaldiv))
