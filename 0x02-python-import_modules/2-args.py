591ecdccbdc442be3b05095b965a323733b0c508
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 19:42:09 +0200

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+if __name__ == '__main__':
+    arguments = len(argv)
+    if arguments > 2:
+        print(f"{arguments - 1} arguments:")
+        for i in range(1, arguments):
+            print(f"{i}: {argv[i])
+
+    if arguments == 2:
+        print(f"{arguments - 1} argument:")
+        print(f"1: {argv[1])
+
+    if arguments < 2:
+        print(f"0 arguments.")
