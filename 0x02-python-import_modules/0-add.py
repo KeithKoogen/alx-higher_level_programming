a93614791e6468eb15e31a809ed51329497ffb95
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-01 19:00:43 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     from add_0 import add
     a = 1
     b = 2
-    sum = add(a,b)
-    print(f"{a} + {b} = {sum}")
+    sum2 = add(a,b)
+    print("{num1} + {num2} = {sum1}".format(num1 = a, num2 = b, sum1 = sum2))
 
