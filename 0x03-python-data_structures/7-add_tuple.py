8e0cb6a3e761552efbeb97af05354f66648ab76a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-05 16:09:16 +0200

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,13 +6,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a = (tuple_a[0], 0)
     else:
         a = (0, 0)
-     
+
     if len(tuple_b) >= 2:
         b = (tuple_b[0], tuple_b[1])
     elif len(tuple_b) == 1:
         b = (tuple_b[0], 0)
     else:
         b = (0, 0)
-            
+
     new_tuple = (a[0] + b[0], a[1] + b[1])
     return new_tuple
