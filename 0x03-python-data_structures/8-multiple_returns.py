42ff1e200bd839041608b24cc414531301c00946
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-05 16:18:38 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length != 0:
+    if length > 0:
         new_tuple = (sentence[0], length)
     else:
         new_tuple = (None, 0)
