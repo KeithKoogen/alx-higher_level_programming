8d908482885916a8bf26969e1300776a7f41f023
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-05 14:53:56 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
-            print("{:d}".format(item), end="")
+            if item != row[-1]:
+                print("{:d}".format(item), sep=" ", end="")
+            else:
+                print("{:d}".format(item), end="")
         print("")
