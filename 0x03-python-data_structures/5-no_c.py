108188ba68f144bc1ffb6d234b907a16357ae5ac
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-05 14:14:56 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ""
     for i in range(len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
-            new_string = newstring + my_string[i]
+            new_string = new_string + my_string[i]
     return new_string
