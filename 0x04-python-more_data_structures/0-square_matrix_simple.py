c5762a0b301f6dfda1159275db3f3ee16d720412
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-07 17:03:04 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,5 @@ def square_matrix_simple(matrix=[]):
     for x in range(len(new_matrix)):
         new_matrix[x] = matrix[x].copy()
         for i in range(len(new_matrix[x])):
-            print(new_matrix[x][i])
             new_matrix[x][i] = new_matrix[x][i] * new_matrix[x][i]
-            print(new_matrix[x][i])
     return new_matrix
