271366f3f2ca435aa6c5782f311c7904f98a3da7
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-07 18:40:39 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best = max(a_dictionary, key=a_dictionary.get)
-    if best == None:
+    if len(a_dictionary.values()) > 0:
+        best = max(a_dictionary, key=a_dictionary.get)
+        return best
+    else:
         return None
-    return best
