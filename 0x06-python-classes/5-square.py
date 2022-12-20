f8d8f525abd0cb0b8701d862b3345fbdf6796f3c
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-20 19:16:18 +0200

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""defines a class called Square"""
+
+
+class Square:
+    """Initialize a Square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Define the size property of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set the size property of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return (self.__size * self.__size)
+    
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
