67a71da1aae9550639e3e709364b75721477d02e
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-20 18:46:32 +0200

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defines a class called sqaure"""
+
+
+class Square:
+    """a class called square"""
+
+    def __init__(self, size=0):
+        """initialize the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """returns the current square area"""
+        return (self.__size * self.__size)
