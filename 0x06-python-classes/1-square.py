3df81b2c571e76200e3e0c22208267bdb22ce919
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-20 17:19:20 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Defines a class called square"""
 class Square:
+    """A Square class"""
     def __init__(self, size):
+        """ Initilize the Square class"""
         self.__size = size
