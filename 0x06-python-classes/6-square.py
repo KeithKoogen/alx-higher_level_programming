870eb76ee2e9e665bc88fa3a6b3c585aa9a08de4
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-20 19:27:06 +0200

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Initialize a Square"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.position = position
 
