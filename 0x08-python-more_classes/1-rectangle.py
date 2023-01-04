5234a3db4022eaa431a29fd4427d5690c9d46299
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2023-01-04 11:25:23 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """ A class called Rectangle """
     
- def __init__(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
